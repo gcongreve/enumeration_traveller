1b9dd0998257748efee67c34556c1d583dbbfa5b
Graeme Congreve <junkmailgmc@gmail.com>
2018-11-21 16:26:33 +0000

    first 8 tests passing

--- models/traveller.js
+++ models/traveller.js
@@ -3,15 +3,28 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const startLocations = this.journeys.map((journey) => {
+    const location = journey.startLocation;
+    return location;
+  });
+  return startLocations;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const endLocations = this.journeys.map((journey) => {
+    const location = journey.endLocation;
+    return location
+  })
+  return endLocations;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const journeys = this.journeys.filter((journey) => {
+    if (journey.transport === transport){
+      return journey;
+    }
+  })
+  return journeys
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
